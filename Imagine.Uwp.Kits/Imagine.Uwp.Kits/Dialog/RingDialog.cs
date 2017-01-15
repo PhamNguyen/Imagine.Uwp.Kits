4c9a1bb6ed388a1edb71a6d7e1c7e3979caf0a08
PhamNguyen <pham.nguyen@hotmail.com>
2017-01-15 19:05:34 +0700

    Imagine.Uwp.KIts

---
+++ Imagine.Uwp.Kits/Imagine.Uwp.Kits/Dialog/RingDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Imagine.Uwp.Kits.Dialog
+{
+    public class RingDialog
+    {
+        private Popup popup;
+
+        public void Show() {
+            if (popup == null)
+                popup = new Popup();
+            popup.Width = Window.Current.Bounds.Width;
+            popup.Height = Window.Current.Bounds.Height;
+
+        }
+
+        public void Hide() {
+
+        }
+    }
+}
