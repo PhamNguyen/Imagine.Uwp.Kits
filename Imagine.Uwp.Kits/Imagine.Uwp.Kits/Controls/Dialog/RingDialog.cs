760c56fe38710f6202ebeb963a1a4371a075968d
PhamNguyen <pham.nguyen@hotmail.com>
2017-01-16 01:14:35 +0700

    Imagine.Uwp.Kits.Dialog.RingDialog

--- Imagine.Uwp.Kits/Imagine.Uwp.Kits/Controls/Dialog/RingDialog.cs
+++ Imagine.Uwp.Kits/Imagine.Uwp.Kits/Controls/Dialog/RingDialog.cs
@@ -3,25 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
 
 namespace Imagine.Uwp.Kits.Controls.Dialog
 {
     public class RingDialog
     {
-        private Popup popup;
+        private static Popup popup;
+        private static RingView ring;
 
-        public void Show() {
+        public RingDialog() {
             if (popup == null)
                 popup = new Popup();
-            popup.Width = Window.Current.Bounds.Width;
-            popup.Height = Window.Current.Bounds.Height;
-
+            if(ring == null)
+                ring = new RingView();
         }
 
-        public void Hide() {
+        public void Show()
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
 
+            Window.Current.SizeChanged += Current_SizeChanged;
+            
+            SizeChanged();
+
+            popup.Child = ring;
+
+            popup.IsOpen = true;
+        }
+
+        private void SizeChanged()
+        {
+            popup.Width = Window.Current.Bounds.Width;
+            popup.Height = Window.Current.Bounds.Height;
+            ring.Width = popup.Width;
+            ring.Height = popup.Height;
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            SizeChanged();
+        }
+
+        public void Hide()
+        {
+            popup.IsOpen = false;
         }
     }
 }
